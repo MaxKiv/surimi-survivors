c70f3dc50892b2373b61270913d6a9e0f00e912b
Timon Borg <tborg@bol.com>
2022-10-08 17:41:56 +0200

    FUCKING CAMERA CWOGKDFG

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,8 @@ struct Wall {
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+    let mut width = 800.0;
+    let mut height = width * (y / x);
     let mut game_running = true;
 
     let resources = Resources::new().await.unwrap();
@@ -43,12 +45,27 @@ async fn main() {
     };
 
     let mut wall = Wall{
-        collider: world.add_solid(vec2(170.0, 130.0), 32, 8),
+        collider: world.add_solid(vec2(400.0, 130.0), 32, 8),
         speed: 50.,
     };
 
+    // let camera = Camera2D::from_display_rect(Rect::new(
+    //     (x / 2.0) - (width / 2.0),
+    //     (y / 2.0) - (height / 2.0),
+    //     width,
+    //     height)
+    // );
+
     while game_running {
         clear_background(LIGHTGRAY);
+
+
+
+
+        {
+            let pos = world.solid_pos(wall.collider);
+            draw_texture(resources.crab_sprite, pos.x, pos.y, YELLOW);
+        }
 
         if is_key_down(KeyCode::D) {
             x += 1.0;
@@ -68,6 +85,14 @@ async fn main() {
 
         draw_text(&format!("{} FPS", get_fps()).to_string(), 20.0, 20.0, 20.0, DARKGRAY);
 
+
+        let camera = Camera2D::from_display_rect(Rect::new(
+            x - (width / 2.0),
+            y - (height / 2.0),
+            width,
+            height)
+        );
+        set_camera(&camera);
         draw_texture(resources.crab_sprite, x, y, YELLOW);
 
         next_frame().await
